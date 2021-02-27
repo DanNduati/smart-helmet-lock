6df05d83218da81ee370c98fc259da2f831ae066
Daniel Nduati <dannduati2@gmail.com>
2021-02-27 06:44:14 +0300

    refactored code according to Simon's recommendations

--- arduino/smartHelmet/smartHelmet.ino
+++ arduino/smartHelmet/smartHelmet.ino
@@ -1,139 +1,99 @@
 #include <SoftwareSerial.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#define testLed A0
+#define lock 4
 
-//verification led
-const int led = A0;
-const int Lock = 4;
-// Create MFRC522 instance.
-constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
+//RFID
+constexpr uint8_t RST_PIN = 9;
 constexpr uint8_t SS_PIN = 10;
-
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
-
 //byte array for verified NUID
 byte nuidStored[2][4] = {{83, 232, 253, 20}, {189, 217, 217, 115}};
-//byte array for read card
 byte nuidReadCard[4];
-//bluetooth
+
+//BLUETOOTH
 SoftwareSerial BTSerial(6, 5); // RX | TX 5/6
 char inputByte;
 
-//unlock flag
-bool unlock = false;
-
-
+boolean doorOpened = false;
+boolean successRead = false;
 void setup() {
-  pinMode(lock,OUTPUT);
-  pinMode(led, OUTPUT);
   Serial.begin(9600);
   SPI.begin();
   BTSerial.begin(9600);
+  pinMode(testLed, OUTPUT);
+  pinMode(lock, OUTPUT);
   mfrc522.PCD_Init();
-  //If you set Antenna Gain to Max it will increase reading distance
   mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
-  //digitalWrite(led,0);
 }
-
 
 void loop() {
-  lock();
-  //(!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) ||
-  //!(mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) ||
-  //!BTSerial.available()
-  //if (!BTSerial.available() && !(mfrc522.PICC_IsNewCardPresent()))
-  //return;
-  
-
-  if (btPresent) {
-    if (checkBt()) {
-      unlock = true;
-      openlock();
+  while (!doorOpened) {
+    //check for bt first
+    if (BTSerial.available() > 0) {
+      if (verifyBt(BTSerial.read())) {
+        doorOpened = true;
+        digitalWrite(testLed, 1);
+        digitalWrite(lock,1);
+        delay(1000);
+        digitalWrite(testLed, 0);
+        digitalWrite(lock,0);
+        doorOpened = false;
+      }
+    }
+    
+    //check for rfid
+    else if (getID()) {
+      if (verifyRfid()) {
+        doorOpened = true;
+        digitalWrite(testLed, 1);
+        digitalWrite(lock,1);
+        delay(1000);
+        digitalWrite(testLed, 0);
+        digitalWrite(lock,0);
+        doorOpened = false;
+      }
+    }
+    else {
       return;
     }
-    else {
-      lock();
-      unlock = false;
-    }
-  }
-  else if (rfidPresent()) {
-    if (checkRfid()) {
-      unlock = true;
-      openlock();
-    }
-    else {
-      lock();
-      unlock = false;
-    }
-  }
-  else {
-    return;
-  }
-		
-  }
-
-bool btPresent() {
-  if (BTSerial.available()) {
-    return 1;
-  }
-  else {
-    return 0;
-  }
-}
-bool rfidPresent() {
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    return 1;
-  }
-  else {
-    return 0;
   }
 
 }
-bool checkRfid() {
-  if (!mfrc522.PICC_IsNewCardPresent())
-    return;
-  //digitalWrite(led,HIGH);
-  //ensure the NUID has been read
-  if (!mfrc522.PICC_ReadCardSerial())
-    return;
-  Serial.println("Card Read!!");
-  //verify if the card read is a valid card(2)
+
+uint8_t getID() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return 0;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return 0;
+  }
+  for ( uint8_t i = 0; i < 4; i++) {
+    nuidReadCard[i] = mfrc522.uid.uidByte[i];
+  }
+  mfrc522.PICC_HaltA(); // Stop reading
+  return 1;
+}
+
+bool verifyRfid() {
   if ((mfrc522.uid.uidByte[0] == nuidStored[0][0] && mfrc522.uid.uidByte[1] == nuidStored[0][1] && mfrc522.uid.uidByte[2] == nuidStored[0][2] && mfrc522.uid.uidByte[3] == nuidStored[0][3]) ||
       (mfrc522.uid.uidByte[0] == nuidStored[1][0] && mfrc522.uid.uidByte[1] == nuidStored[1][1] && mfrc522.uid.uidByte[2] == nuidStored[1][2] && mfrc522.uid.uidByte[3] == nuidStored[1][3])) {
-    //match
-    Serial.println("Match!!");
-    //granted();
+    //clear stored nuid
+    for (int i = 0; i < 4; i++) {
+      mfrc522.uid.uidByte[i] = 0;
+    }
     return 1;
   }
   else {
-    //digitalWrite(led,0);
-    //delay(1000);
-    Serial.println("No Match!!");
     return 0;
   }
 }
 
-bool checkBt() {
-  inputByte = BTSerial.read();
-  //Serial.println(inputByte);
-  if (inputByte == 49) {
+bool verifyBt(char inByte) {
+  if (inByte == 49) {
     return 1;
   }
-  else if (inputByte == 48) {
-    return 0;
-  }
 }
 
-void openlock() {
-  digitalWrite(led, 1);
-  digitalWrite(Lock, 1);
-  delay(1000);
-}
-
-
-void lock() {
-  digitalWrite(led, 0);
-  digitalWrite(Lock, 0);
-  delay(1000);
-}
